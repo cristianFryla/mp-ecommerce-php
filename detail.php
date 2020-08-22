214c726c9f15700426b58cfda7046a25a81ebdeb
Cristian Marucci <cristian@avradev.com>
2020-08-22 14:05:36 -0300

    MercadoPago

--- detail.php
+++ detail.php
@@ -8,11 +8,16 @@ if ( !empty($_POST)) {
     $vURLImg = $_POST['img'];
     $vDesc = 'Dispositivo móvil de Tienda e-commerce';
 
-   
+    $actualImg = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+    $actualImg .= substr($vURLImg,2, len($vURLImg)); 
 
     echo '<h1>' . $vTitle . '</h1>';
     echo '<h1>' . $vPrice . '</h1>';
+    echo '<h1>' . $actualImg . '</h1>';
     echo '<h1>' . $vURLImg . '</h1>';
+    echo '<h1>' . substr($vURLImg,2, len($vURLImg)) . '</h1>';
+    
+
     /*
 
     MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
